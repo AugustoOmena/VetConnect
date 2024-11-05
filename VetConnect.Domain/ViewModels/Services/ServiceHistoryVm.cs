0fb34087d387250325e5aac4bd0d74d8d47a613a
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-11-04 21:21:43 -0300

    Add Id to PetVm

--- VetConnect.Domain/ViewModels/Services/ServiceHistoryVm.cs
+++ VetConnect.Domain/ViewModels/Services/ServiceHistoryVm.cs
@@ -2,10 +2,8 @@ using VetConnect.Domain.ViewModels.Pets;
 
 namespace VetConnect.Domain.ViewModels.Services;
 
-public class ServiceHistoryVm
+public class ServiceHistoryVm : BaseVm
 {
-    public Guid Id { get; set; }
-    
     public string Name { get; set; }
     
     public string Description { get; set; }
