4f6c05e762449f79941920ac9e410f1dab2366c3
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-11-05 22:01:38 -0300

    Removing password property of userVm

--- VetConnect.Domain/ViewModels/Users/UserVm.cs
+++ VetConnect.Domain/ViewModels/Users/UserVm.cs
@@ -6,11 +6,13 @@ namespace VetConnect.Domain.ViewModels.Users;
 public class UserVm : BaseVm
 {
     public string? Email { get; set; }
+    
     public string FirstName { get; set; }
+    
     public string LastName { get; set; }
     
     public string? Phone { get; set; }
-    public string Password { get; set; }
+    
     public EUserType UserType { get; set; }
     
     //Relacionamento 1:N - Um User pode ter vários Pets
